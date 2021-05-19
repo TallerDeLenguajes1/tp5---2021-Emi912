7d78a6d41bc131eaaabd29eff8d7d09ec9d55917
Emilio Merchan <emimerchan83@gmail.com>
2021-05-19 16:58:20 -0300

    Primer ejercicio resuelto

---
+++ TP5/TP5/TP5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero entero");
+            int num;
+            int numinv = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                while(num > 0)
+                {
+                    numinv = numinv * 10 + (num % 10);
+                    num = num / 10;
+                }
+                Console.Write("El numero invertido es " + numinv);
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un numero mayor a 0");
+            }
+
+        }
+
+    }
+}

5ba7d3a2eb5eab545f1defeb129f163a554035d2
Emilio Merchan <emimerchan83@gmail.com>
2021-05-21 23:42:46 -0300

    Falta terminar el punto 4

--- TP5/TP5/TP5/Program.cs
+++ TP5/TP5/TP5/Program.cs
@@ -7,14 +7,76 @@ namespace TP5
         static void Main(string[] args)
         {
             //Ejercicio1();
-            Ejercicio2(); 
+            //Ejercicio2();
+            //Ejercicio3();
+            Ejercicio4();
+
+        }
+
+        private static void Ejercicio4()
+        {
+            String Cadena;
+            String Cadena2;
+            Console.Write("Escriba una cadena por favor!!!\n");
+            Cadena = Console.ReadLine();
+            //Console.Write("El tamaño de la cadena que escribiste es " + Cadena.Length + "\n");
+            //Console.Write("Escriba otra cadena para concatenarla con la primera\n");
+            //Cadena2 = (Console.ReadLine());
+            //Console.Write("La cadena resultante es " + string.Concat(Cadena, Cadena2));
+            //Console.Write("Una parte de tu cadena es " + Cadena.Substring(2,4) + "\n");
+            //foreach(char c in Cadena)
+            //{
+            //    Console.WriteLine(c);
+
+            //}
+            int aux;
+            aux = Cadena.IndexOf("hola");
+            if(aux == -1)
+            {
+                Console.WriteLine("No se Encontro la palabra");
+            }
+            else
+            {
+                Console.WriteLine("Se Encontro la palabra");
+
+            }
+
+
+
+
+        }
+
+        private static void Ejercicio3()
+        {
+            double num;
+            int a;
+            int b;
+            Console.WriteLine("Escriba un numero para calcular");
+            num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"El valor absoluto del numero que ingresaste es {Math.Abs(num)}");
+            Console.WriteLine($"El cuadrado del numero que ingresaste es {Math.Pow(num, 2)}");
+            Console.WriteLine($"La raiz del numero que ingresaste es {Math.Sqrt(num)}");
+            Console.WriteLine($"El Seno del numero que ingresaste es {Math.Sin(num)}");
+            Console.WriteLine($"El Coseno del numero que ingresaste es {Math.Cos(num)}");
+            Console.WriteLine($"El numero redondeado que ingresaste es {Math.Round(num)}");
+
+            Console.WriteLine("Escriba dos numeros ahora para compararlos");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"El mas grande entre esos dos numeros es {Math.Max(a, b)}");
+            Console.WriteLine($"El mas chico entre esos dos numeros es {Math.Min(a, b)}");
+
+
+
+
 
         }
 
         private static void Ejercicio2()
         {   
-            int a;
-            int b;
+            float a;
+            float b;
             int operador;
             int aux = 0;
             do
